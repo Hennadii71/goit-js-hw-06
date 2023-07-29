4fdcd56b41452d81d1196755c109fb778ddf26c7
Hennadii71 <genadisema@gmail.com>
2023-07-29 10:08:25 +0300

    Update task 09

--- js/task-09.js
+++ js/task-09.js
@@ -2,4 +2,16 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+
+}
+
+const buttonChangeColor = document.querySelector('.change-color');
+const bodyColor = document.querySelector('body');
+const meanColor = document.querySelector('.color');
+
+buttonChangeColor.addEventListener('click', getColorChange);
+
+function getColorChange(){
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  meanColor.textContent = getRandomHexColor();
 }
