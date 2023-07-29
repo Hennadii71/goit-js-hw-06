2e57f3a5d9fc969bf16b830a1e0785f61f0a33c9
Hennadii71 <genadisema@gmail.com>
2023-07-29 08:30:16 +0300

    Update task 08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', onFormLogin);
+
+function onFormLogin(event){
+  event.preventDefault();
+
+   const {email,password} = event.currentTarget.elements;
+   const objectInput = {
+   email: email.value,
+   password: password.value,}
+
+   console.log(objectInput);
+
+  if(email.value === '' || password.value === ''){
+    alert('Please fill in all fields');
+}
+    
+  event.currentTarget.reset();
+}
