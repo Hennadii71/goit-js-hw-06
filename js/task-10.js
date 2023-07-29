0473629222cd68887d7229327a2eb56fb66f61e8
Hennadii71 <genadisema@gmail.com>
2023-07-29 12:50:08 +0300

    Update task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+// const inputNumber = document.querySelector('input');
+// const buttonCreate = document.querySelector('[data-create]');
+// const buttonDestroy = document.querySelector('[data-destroy]');
+// const boxesCreate = document.querySelector('#boxes');
+
+// buttonCreate.addEventListener('click', createBoxes(amount));
+// buttonDestroy.addEventListener('click', destroyBoxes());
+
+// function createBoxes(amount){
+//  }
+
+
+// В процессі.
